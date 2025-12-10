308e24bffa6b8dd2ab0546c3682e5fadd4322207
MahiKalavadia <mahikalavadia0612@gmail.com>
2025-12-10 22:06:50 +0530

    Input output file added

---
+++ inputoutput.py
@@ -0,0 +1,23 @@
+# input :- print function is used for input
+print("Hello World!!")
+
+# python can have many statements
+print("Hello")
+print("How are you?")
+print("Remember me?")
+
+#Print Without a New Line
+print("Hello World!", end=" ")
+print("I will print on the same line.")
+
+# Print Numbers
+print(3)
+print(358)
+print(50000)
+
+#You can also do math inside the print() function:
+print(3 + 3)
+print(2 * 5)
+
+#Mix Text and Numbers
+print("I am", 35, "years old.")
